e954ff827c5c274b97aa4fe8e0af10d04ec8eee5
Evan Anderson <evanmand02@gmail.com>
2025-02-08 22:21:28 -0700

    Hamburger styling and transitions

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 function navOpen() {
     const hamburger = document.getElementById("hamburger");
+    const M = document.getElementById("M")
+    const L = document.getElementById("L")
+    const R = document.getElementById("R")
     const isOpen = hamburger.dataset.isOpen;
-    console.log(isOpen)
+    console.log(isOpen);
     if (isOpen === 'false') {
         hamburger.style.transform = "rotate(90deg)";
         hamburger.dataset.isOpen = 'true';
+        M.style.width = '42.7%';
+        L.style.width = '26.4%';
+        R.style.width = '30.9%';
     }
     else {
         hamburger.dataset.isOpen = 'false';
         hamburger.style.transform = "rotate(0deg)";
+        M.style.width = '52.8%';
+        L.style.width = '32.6%';
+        R.style.width = '14.6%';
     }
 }
